97f74a1353ce7edee2a590f762c56584135e9846
ragaey <mragaey@arkdev.net>
2019-07-07 15:38:26 +0200

    Adding Deployment script

---
+++ build/precache-manifest.378f5dd0b97d307d0fc56e61e9d6cfcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "400856da7cb16ba339580f26fb76fb41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca2a62e9efb81759e5c5",
+    "url": "/static/css/main.be97952b.chunk.css"
+  },
+  {
+    "revision": "d9501fd6844b3df020ef",
+    "url": "/static/js/2.a1db30ca.chunk.js"
+  },
+  {
+    "revision": "ca2a62e9efb81759e5c5",
+    "url": "/static/js/main.a7a5cc24.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react"
 import logo from "./logo.svg"
 import "./App.css"
 import CssBaseline from "@material-ui/core/CssBaseline"
+import SEO from "./components/seo"
 
 function App() {
   return (
     <div className="App">
       <CssBaseline/>
+      <SEO title='Home' description='Mohamed Ragaey UI Developer with a good sense of humor. '/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
         <p>
