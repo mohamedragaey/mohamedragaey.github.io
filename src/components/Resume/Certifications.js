a20c0e0b1e4bbd91e929f9f6e8d3278251815e4b
mohamedragaey <mragaey@arkdev.net>
2020-10-18 21:49:20 +0200

    Updating Certifications section.

--- src/components/Resume/Certifications.js
+++ src/components/Resume/Certifications.js
@@ -8,8 +8,8 @@ const Certifications = () => {
         <div className="sectiontitle">Certifications</div>
       </div>
       <div className="paragraph">
-        <div>
-          <p>React Nanodegree by Udacity [https://confirm.udacity.com/ULHK2DC2]</p></div>
+        <div><p>React Nanodegree by Udacity [https://confirm.udacity.com/ULHK2DC2]</p></div>
+        <div><p>React Certificate by Hackerrank [https://www.hackerrank.com/certificates/3e2f858760ea]</p></div>
       </div>
     </div>
   )
